a4e88beb4bbbd00dd930b39f681cf29f59f94e6a
JoshuaMFz <joshuak6k@gmail.com>
2023-07-26 14:09:38 -0400

    26/07/2023

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # CLIENTE -> REQUEST | SERVIDOR -> RESPONSE
 # HTTP Request
@@ -8,9 +7,4 @@ def home(request):
         'name': 'Luiz'
     })
 
-def contato(request):
-    return render(request, 'temp/temp.html')
-
-def sobre(request):
-    return HttpResponse('sobre')  
 
